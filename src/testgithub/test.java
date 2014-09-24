9ee3c09374b390d087155fcd493d9c3ebe59bbc6
LCLing <wait_for_happiness@163.com>
2014-09-24 15:54:20 +0800

    Update test.java

--- src/testgithub/test.java
+++ src/testgithub/test.java
@@ -5,6 +5,6 @@ public class test {
 	public static void main(String[] args) {
 		System.out.println("hello github!");
 		
-		System.out.println("changed from Web!")
+		System.out.println("changed from Web!");
 	}
 }
